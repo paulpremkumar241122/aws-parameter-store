aec3dbe12b797d2384894b876a2c0ac921a51c19
Paul Prem Kumar <paul.prem5@gmail.com>
2024-01-07 03:17:58 +0530

    amazon.aws.aws_ssm .tf state

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
-resource "aws_ssm_parameter" "Params" {
-  count = length(var.Params)
-  name  = var.Params[count.index].name
-  type  = var.Params[count.index].type
-  value = var.Params[count.index].value
+resource "aws_ssm_parameter" "params" {
+  count = length(var.params)
+  name  = var.params[count.index].name
+  type  = var.params[count.index].type
+  value = var.params[count.index].value
 }
 
-variable "Params" {
+variable "params" {
   default = [
     {name = "demo1", value = "demo1", type = "string" },
     {name = "demo2", value = "demo2", type = "string" }
