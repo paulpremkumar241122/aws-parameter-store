179bad9e211c2d3c7f536857d92bbae68c987ecd
Paul Prem Kumar <paul.prem5@gmail.com>
2024-01-26 07:17:00 +0530

    rabbaitmq parameters updated - ampq user and passswords

--- main.tf
+++ main.tf
@@ -28,10 +28,13 @@ variable "params" {
     { name = "roboshop.dev.user.mongo", value = "true", type = "String" },
     { name = "roboshop.dev.user.redis_host", value = "redis-dev.vagdevi.store", type = "String" },
     { name = "roboshop.dev.user.mongo_url", value = "mongodb://mongodb-dev.vagdevi.store:27017/users", type = "String" },
-
+    { name = "roboshop.dev.rabbitmq.amqp_user", value = "roboshop", type = "String" },
 
 
     #### Passwords will not be stored in git repos, In the companies it will be created manually . who have access will store this secret in parameter store....
-    { name = "roboshop.dev.payment.amqp_pass", value = "roboshop123", type = "SecureString" },
+    { name = "roboshop.dev.payment.amqp_password", value = "roboshop123", type = "SecureString" },
+    { name = "roboshop.dev.rabbitmq.amqp_pass", value = "roboshop123", type = "SecureString" },
+
+
   ]
 }
