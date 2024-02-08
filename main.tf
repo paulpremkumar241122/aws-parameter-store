d3cf0ccbf9ecadcac6bac0fdb3240e2e592c88a5
Paul Prem Kumar <paul.prem5@gmail.com>
2024-02-09 04:01:55 +0530

    8080-80 in frontend, 80,443, http redirect

--- main.tf
+++ main.tf
@@ -11,11 +11,12 @@ resource "aws_ssm_parameter" "params" {
 
 variable "params" {
   default = [
-    { name = "roboshop.dev.frontend.catalogue_url", value = "http://catalogue-dev.vagdevi.store:8080/", type = "String" },
-    { name = "roboshop.dev.frontend.user_url", value = "http://user-dev.vagdevi.store:8080/", type = "String" },
-    { name = "roboshop.dev.frontend.cart_url", value = "http://cart-dev.vagdevi.store:8080/", type = "String" },
-    { name = "roboshop.dev.frontend.shipping_url", value = "http://shipping-dev.vagdevi.store:8080/", type = "String" },
-    { name = "roboshop.dev.frontend.payment_url", value = "http://payment-dev.vagdevi.store:8080/", type = "String" },
+    { name = "roboshop.dev.frontend.catalogue_url", value = "https://catalogue-dev.vagdevi.store/", type = "String" },
+    { name = "roboshop.dev.frontend.user_url", value = "https://user-dev.vagdevi.store/", type = "String" },
+    { name = "roboshop.dev.frontend.cart_url", value = "https://cart-dev.vagdevi.store/", type = "String" },
+    { name = "roboshop.dev.frontend.shipping_url", value = "https://shipping-dev.vagdevi.store/", type = "String" },
+    { name = "roboshop.dev.frontend.payment_url", value = "https://payment-dev.vagdevi.store/", type = "String" },
+
     { name = "roboshop.dev.cart.redis_host", value = "redis-dev.vagdevi.store", type = "String" },
     { name = "roboshop.dev.cart.catalogue_host", value = "catalogue-dev.vagdevi.store", type = "String" },
     { name = "roboshop.dev.cart.catalogue_port", value = "8080", type = "String" },
